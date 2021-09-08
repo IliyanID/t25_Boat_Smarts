c7a2709d42c9d14ffd7d8d2495e8689c5a6f61b1
CalebCluett <ccluett@colostate.edu>
2021-09-07 20:56:05 -0600

    Added Bio,Name,Hometown, and Image

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -11,10 +11,10 @@ export const teamData =
 
 export const memberData = [
     {
-        name: "Team Member 1",
-        bio: "Bio Here",
-        homeTown: "Hometown",
-        imagePath: memberPic
+        name: "Caleb Cluett",
+        bio: "Caleb is going for a degree in Computer Science with a consentration in Networking and Secuitry. Caleb has other interests which include, includes Rock Climbing, Woodworking, and Reptiles",
+        homeTown: "Fort. Collins, Colorado",
+        imagePath: "../../static/images/CalebPic.jpg"
     },
     {
         name: "Team Member 2",
