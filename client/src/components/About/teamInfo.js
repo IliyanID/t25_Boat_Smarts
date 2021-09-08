5200baabd365613407ed7a3b8fdf9835928ce627
Andrew Holmes <holmesaw@rams.colostate.edu>
2021-09-08 18:21:20 +0000

    Closes #32

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -1,5 +1,6 @@
 import memberPic from "../../static/images/Placeholder.jpg";
 import teamPic from "../../static/images/PlaceholderTeam.jpg";
+import andrewPic from "../../static/images/AndrewPhoto.jpg";
 
 export const teamData =
     {
@@ -29,10 +30,10 @@ export const memberData = [
         imagePath: memberPic
     },
     {
-        name: "Team Member 4",
-        bio: "Bio Here",
-        homeTown: "Hometown",
-        imagePath: memberPic
+        name: "Andrew Holmes",
+        bio: "Andrew is a 4th year Computer Science and Mathematics Education student at CSU. He has interests in algorithm analysis, artificial intelligence, and pedagogy. After finishing his bachelor's degree, Andrew plans on pursuing a master's degree and teaching Computer Science at a university level.",
+        homeTown: "Highlands Ranch, Colorado",
+        imagePath: andrewPic
     },
     {
         name: "Team Member 5",
