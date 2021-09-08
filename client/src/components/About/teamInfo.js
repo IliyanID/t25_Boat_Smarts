7cbb584859fbf1523b7496b6ce560aada0f8bdfa
IliyanID <dev.iliyan.dimitrov@gmail.com>
2021-09-08 13:42:38 -0600

    Added Bio To About

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -1,13 +1,13 @@
 import memberPic from "../../static/images/Placeholder.jpg";
-import teamPic from "../../static/images/PlaceholderTeam.jpg";
+import teamPic from "../../../../team/images/25.jpg";
+import IliyanDimitrov from "../../static/images/Iliyan_Dimitrov_Bio_Picture.png"
 
 export const teamData =
     {
-        teamName: "Team Name",
+        teamName: "The Boat Smarts",
         missionStatement: "Mission Statement",
         imagePath: teamPic,
     };
-
 
 export const memberData = [
     {
@@ -29,10 +29,10 @@ export const memberData = [
         imagePath: memberPic
     },
     {
-        name: "Team Member 4",
-        bio: "Bio Here",
-        homeTown: "Hometown",
-        imagePath: memberPic
+        name: "Iliyan Dimitrov",
+        bio: "Iliyan is a third year Computer Science student with a focus in AI and Machine Learning at CSU. His interests consists of  web development, game development, and artificial intelligence development. After graduating, Iliyan would like to be a software developer within one of the big four FANG companies.",
+        homeTown: "Parker",
+        imagePath: IliyanDimitrov
     },
     {
         name: "Team Member 5",
