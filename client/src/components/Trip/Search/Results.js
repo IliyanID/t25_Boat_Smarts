d7de5b4d769758e9e028bdb9c0f7ef8d64518ce2
Derek McCracken <dsmccracken@outlook.com>
2021-09-15 08:58:59 -0600

    add details to result card

--- client/src/components/Trip/Search/Results.js
+++ client/src/components/Trip/Search/Results.js
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
-import {ListGroup, ListGroupItem, UncontrolledCollapse} from 'reactstrap';
+import {
+    ListGroup, 
+    ListGroupItem, 
+    UncontrolledCollapse, 
+    Card, 
+    CardBody, 
+    CardHeader
+} from 'reactstrap';
+import {useToggle} from '../../../hooks/useToggle';
 
 export default function Results () {
     const results = props.searchResults;
@@ -10,16 +18,33 @@ export default function Results () {
         <ListGroup>
             {places.map((place, i) => (
                 <ListGroupItem>
-                    <SinglePlace place={place} data-index={i} />
+                    <SinglePlace place={place} index={i} />
                 </ListGroupItem>
             ))}
         </ListGroup>
     );
 }
 
-function SinglePlace() {
+function SinglePlace(props) {
+    const [isOpen, toggle] = useToggle(false);
+    const toggleId = "toggle" + props.index;
+    const toggleSelector = "#" + toggleId;
+
     return (
-        <>
-        </>
+        <Card id={toggleId}>
+            <CardHeader>{place.name}</CardHeader>
+            <CardBody>
+                <UncontrolledCollapse toggler={toggleSelector}>
+                    <p><strong>Municipality:</strong> {place.municipality}</p>
+                    <p><strong>Country:</strong> {place.country}</p>
+                    <p><strong>Region:</strong> {place.region}</p>
+                    <br />
+                    <p><strong>Coordinates:</strong> {place.latitude}, {place.longitude}</p>
+                    <p><strong>Altitude:</strong> {place.altitude}</p>
+                    <br />
+                    <p><strong>URL:</strong> {place.url}</p>
+                </UncontrolledCollapse>
+            </CardBody>
+        </Card>
     );
 }
