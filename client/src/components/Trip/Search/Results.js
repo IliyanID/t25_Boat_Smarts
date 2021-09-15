1f0aa4495296c7ab0a3b108765f233a8ed7fda9e
Derek McCracken <dsmccracken@outlook.com>
2021-09-14 20:03:13 -0600

    create Results.js

---
+++ client/src/components/Trip/Search/Results.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {ListGroup, ListGroupItem} from 'reactstrap';
+
+export default function Results () {
+    const results = props.searchResults;
+
+    return (
+        <ListGroup>
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+        </ListGroup>
+    );
+}
