f23f9babb3364162dde2c6c8700ec5603e4e51c8
IliyanID <dev.iliyan.dimitrov@gmail.com>
2021-09-16 14:50:10 -0600

    Fixed bug where client doesn't warn user with popup message on missing features

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Collapse } from 'reactstrap';
 import Header from './Margins/Header';
 import Footer from './Margins/Footer';
+import { missingFeatures, missingFeaturesExists } from './Margins/ServerSettings'
 import About from './About/About';
 import Planner from './Trip/Planner';
 import { useToggle } from '../hooks/useToggle';
 import { LOG } from '../utils/constants';
-import { getOriginalServerUrl, sendAPIRequest, SCHEMAS } from '../utils/restfulAPI';
+import { getOriginalServerUrl, sendAPIRequest } from '../utils/restfulAPI';
 
-export let missingFeatures = []
+
 export default function Page(props) {
 	const [showAbout, toggleAbout] = useToggle(false);
 	const [serverSettings, processServerConfigSuccess] = useServerSettings(props.showMessage);
@@ -25,6 +26,7 @@ export default function Page(props) {
 				</Collapse>
 			</div>
 			<Footer
+				showMessage={props.showMessage}
 				serverSettings={serverSettings}
 				processServerConfigSuccess={processServerConfigSuccess}
 			/>
@@ -46,25 +48,10 @@ function useServerSettings(showMessage) {
 		setServerUrl(url);
 	}
 
-	function missingFeaturesExists(configResponse){
-		let existingClientFeatures = {...SCHEMAS}
-
-		//Uncomment Below to cause missing feature error
-		//existingClientFeatures['where'] = 'S'
-
-		for(let feature in existingClientFeatures){
-			if(!configResponse.features.includes(feature))
-				missingFeatures.push(feature)
-		}
-
-		return missingFeatures.length > 0
-	}
-
 	async function sendConfigRequest() {
 		const configResponse = await sendAPIRequest({ requestType: "config" }, serverUrl);
 		if (configResponse) {
-
-			if(missingFeaturesExists(configResponse)){
+			if(missingFeaturesExists(configResponse,configResponse)){
 				let message = 'Server is missing features [' + missingFeatures.map((feature)=>{return feature}) + ']. Check the log for more details.';
 				try{
 					showMessage(message,"warning")
@@ -73,6 +60,7 @@ function useServerSettings(showMessage) {
 					LOG.error("Failed to show warning message")
 				}
 			}
+
 			processServerConfigSuccess(configResponse, serverUrl);
 		} else {
 			setServerConfig(null);
