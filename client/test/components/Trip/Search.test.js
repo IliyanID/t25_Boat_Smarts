2b2af5f83def5c9690c4bb57243be2067300f662
Derek McCracken <dsmccracken@outlook.com>
2021-09-18 17:09:37 -0600

    add search component tests

--- client/test/components/Trip/Search.test.js
+++ client/test/components/Trip/Search.test.js
@@ -1,15 +1,23 @@
 import '../../jestConfig/enzyme.config.js';
 
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import {describe, expect, it} from '@jest/globals';
 import Search from '../../../src/components/Trip/Search/Search.js';
 
 describe('Search', () => {
 
-    it('sends valid find request', () => {
+    it('renders to screen', async () => {
         const {getByRole} = render(<Search />);
-        
+        expect(getByRole('textbox').value).toBe('');
+        expect(getByRole('search').textContent).toBe('Search');
+    });
+
+    it('updates user input on input', () => {
+        const {getByRole} = render(<Search />);
+        fireEvent.change(getByRole('textbox'), {target: {value: 'Denver'}});
+        expect(getByRole('textbox').value).toBe('Denver');
+        fireEvent.click(getByRole('search'));
     });
 
 });
