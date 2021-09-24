a7d313911d4fafdaa5a814025e551a4b060fade3
Derek McCracken <dsmccracken@outlook.com>
2021-09-24 09:26:10 -0600

    add Results tests

---
+++ client/test/components/Trip/Results.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render, screen,fireEvent} from '@testing-library/react';
+import {describe, expect, it} from '@jest/globals';
+import Results from '../../../src/components/Trip/Search/Results.js';
+import { MOCK_PLACES } from '../../sharedMocks.js';
+
+describe('Results', () => {
+
+    const MOCK_RESULTS = {
+        places: MOCK_PLACES,
+        placesFound: 2
+    }
+
+    it('renders to screen', async () => {
+        render(<Results searchResults={MOCK_RESULTS} />);
+    });
+
+});
