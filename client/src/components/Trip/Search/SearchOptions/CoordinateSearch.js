2ebd00eeed821da06fef8575fce52a898647cb92
Derek McCracken <dsmccracken@outlook.com>
2021-09-24 12:41:23 -0600

    add select for coordinate options

--- client/src/components/Trip/Search/SearchOptions/CoordinateSearch.js
+++ client/src/components/Trip/Search/SearchOptions/CoordinateSearch.js
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
-import {Input, Button, Label, Row, Col} from 'reactstrap';
-import {placeToLatLng} from "../../../../utils/transformers";
+import { Input, Button, Label, Row, Col } from "reactstrap";
+import { placeToLatLng } from "../../../../utils/transformers";
 import { reverseGeocode } from "../../../../utils/reverseGeocode";
 
 export default function CoordinateSearch(props) {
@@ -9,42 +9,71 @@ export default function CoordinateSearch(props) {
     const [longitude, setLongitude] = useState("");
     const [location, setLocation] = useState({});
 
-    function handleLatitudeChange(e) {
+    async function handleLatitudeChange(e) {
         setLatitude(e.target.value);
     }
 
-    function handleLongitudeChange(e) {
+    async function handleLongitudeChange(e) {
         setLongitude(e.target.value);
     }
 
-    function handleClick() {
+    async function handleClick(e) {
+        e.preventDefault();
         getResults();
     }
 
     async function getResults() {
-        setLocation(placeToLatLng({latitude:latitude, longitude:longitude}));
-        const coordsAndName = reverseGeocode(location);
+        setLocation(placeToLatLng({ latitude: latitude, longitude: longitude }));
+        if (location) {
+            const coordsAndName = await reverseGeocode(location);
+        }
         // use this for centering map and info in popup
     }
 
+    useEffect(() => {
+        setLatitude(latitude);
+        setLongitude(longitude);
+    });
+
     return (
         <>
-        <Row>
-            <Col sm="12" md="6" className="mb-1 mt-2">
-                <Label for="latitude">Latitude:</Label>
-                <Input id="latitude" name="latitude" value={latitude} onChange={handleLatitudeChange} placeholder="Latitude"/>
-            </Col>
-            <Col sm="12" md="6" className="mb-1 mt-2">
-                <Label for="latitude">Longitude:</Label>
-                <Input id="longitude" name="longitude" value={longitude} onChange={handleLongitudeChange} placeholder="Longitude"/>
-            </Col>
-        </Row>
-        <Row>
-            <Col className="mt-3">
-                <Button onClick={handleClick}>Find</Button>
-            </Col>
-        </Row>
-        
+            <Row>
+                <Col sm="12" md="6" className="mb-1 mt-2">
+                    <Label for="latitude">Latitude:</Label>
+                    <Input
+                        id="latitude"
+                        name="latitude"
+                        value={latitude}
+                        onChange={handleLatitudeChange}
+                        placeholder="Latitude"
+                    />
+                </Col>
+                <Col sm="12" md="6" className="mb-1 mt-2">
+                    <Label for="latitude">Longitude:</Label>
+                    <Input
+                        id="longitude"
+                        name="longitude"
+                        value={longitude}
+                        onChange={handleLongitudeChange}
+                        placeholder="Longitude"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col className="mt-3 col-auto mr-auto">
+                    <select
+                        className=""
+                        name="coordinateSearchType"
+                        id="coordinateSearchType"
+                    >
+                        <option value="decimal">Decimal Degrees</option>
+                        <option value="dms">Degrees, Minutes, Seconds</option>
+                    </select>
+                </Col>
+                <Col className="mt-3 col-auto">
+                    <Button onClick={handleClick}>Find</Button>
+                </Col>
+            </Row>
         </>
     );
 }
