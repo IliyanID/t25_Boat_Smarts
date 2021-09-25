4d42dfc030d2693289e315c99bb7041fbeb62275
Derek McCracken <dsmccracken@outlook.com>
2021-09-24 20:48:20 -0600

    add coordinate search types

--- client/src/components/Trip/Search/SearchOptions/CoordinateSearch.js
+++ client/src/components/Trip/Search/SearchOptions/CoordinateSearch.js
@@ -3,18 +3,19 @@ import React, { useState, useEffect } from "react";
 import { Input, Button, Label, Row, Col } from "reactstrap";
 import { placeToLatLng } from "../../../../utils/transformers";
 import { reverseGeocode } from "../../../../utils/reverseGeocode";
+import DefaultCoordinateSearch from "./DefaultCoordinateSearch";
+import DegreesMinutesSeconds from "./DegreesMinutesSeconds";
 
 export default function CoordinateSearch(props) {
     const [latitude, setLatitude] = useState("");
     const [longitude, setLongitude] = useState("");
     const [location, setLocation] = useState({});
+    const [searchType, setSearchType] = useState("decimal");
 
-    async function handleLatitudeChange(e) {
-        setLatitude(e.target.value);
-    }
 
-    async function handleLongitudeChange(e) {
-        setLongitude(e.target.value);
+
+    function handleSelectChange(e) {
+        setSearchType(e.target.value);
     }
 
     async function handleClick(e) {
@@ -22,9 +23,9 @@ export default function CoordinateSearch(props) {
         getResults();
     }
 
-    async function getResults() {   
+    async function getResults() {
         if (location) {
-            const coordsAndName = await reverseGeocode(location);
+            const coordDetails = await reverseGeocode(location);
         }
         // use this for centering map and info in popup
     }
@@ -36,26 +37,11 @@ export default function CoordinateSearch(props) {
     return (
         <>
             <Row>
-                <Col sm="12" md="6" className="mb-1 mt-2">
-                    <Label for="latitude">Latitude:</Label>
-                    <Input
-                        id="latitude"
-                        name="latitude"
-                        value={latitude}
-                        onChange={handleLatitudeChange}
-                        placeholder="Latitude"
-                    />
-                </Col>
-                <Col sm="12" md="6" className="mb-1 mt-2">
-                    <Label for="latitude">Longitude:</Label>
-                    <Input
-                        id="longitude"
-                        name="longitude"
-                        value={longitude}
-                        onChange={handleLongitudeChange}
-                        placeholder="Longitude"
-                    />
-                </Col>
+                {(searchType === "decimal" && <DefaultCoordinateSearch setLatitude={setLatitude} setLongitude={setLongitude} />) ||
+                    <>
+                        <DegreesMinutesSeconds latitude={latitude} setLatitude={setLatitude} coordType="Latitude" longitude={longitude} setLongitude={setLongitude} />
+                        <DegreesMinutesSeconds longitude={longitude} setLongitude={setLongitude} coordType="Longitude" latitude={latitude} setLatitude={setLatitude} />
+                    </>}
             </Row>
             <Row>
                 <Col className="mt-3 col-auto mr-auto">
@@ -63,6 +49,7 @@ export default function CoordinateSearch(props) {
                         className=""
                         name="coordinateSearchType"
                         id="coordinateSearchType"
+                        onChange={handleSelectChange}
                     >
                         <option value="decimal">Decimal Degrees</option>
                         <option value="dms">Degrees, Minutes, Seconds</option>
