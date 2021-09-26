a8cf30c1c46323fd3bb4ccce8745538de844664b
Derek McCracken <dsmccracken@outlook.com>
2021-09-26 11:53:43 -0600

    extract single input component

--- client/src/components/Trip/Search/SearchOptions/DegreesMinutesSeconds.js
+++ client/src/components/Trip/Search/SearchOptions/DegreesMinutesSeconds.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import {Col, Label, InputGroup, Input, InputGroupAddon, InputGroupText} from 'reactstrap';
+import React, { useState, useEffect } from "react";
+import { Col, Label, InputGroup, Input, InputGroupAddon, InputGroupText } from 'reactstrap';
 
 
 export default function DMS(props) {
@@ -7,7 +7,7 @@ export default function DMS(props) {
     const [degrees, setDegrees] = useState("");
     const [minutes, setMinutes] = useState("");
     const [seconds, setSeconds] = useState("");
-    
+
     function handleDegreeChange(e) {
         setDegrees(e.target.value);
     }
@@ -34,58 +34,33 @@ export default function DMS(props) {
     }
 
     function toDecimalDegrees() {
-        const decimal = parseFloat(degrees) + parseFloat(minutes)/60 + parseFloat(seconds)/3600;
+        const decimal = parseFloat(degrees) + parseFloat(minutes) / 60 + parseFloat(seconds) / 3600;
         return decimal.toString();
     }
 
     return (
         <>
-        
-        <Col className="mb-1 pr-0">
-            <InputGroup size="small">
-            <Input
-                className="pr-0"
-                id="degrees"
-                name="degrees"
-                value={degrees}
-                onChange={handleDegreeChange}
-                placeholder="Degrees"
-            />
-            <InputGroupAddon addonType="append">
-                <InputGroupText>&#176;</InputGroupText>
-            </InputGroupAddon>
-            </InputGroup>
-        </Col>
-        <Col className="mb-1 px-0 px-sm-1 px-md-2">
-            <InputGroup size="small">
-            <Input
-                className="pr-0"
-                id="minutes"
-                name="minutes"
-                value={minutes}
-                onChange={handleMinuteChange}
-                placeholder="Minutes"
-            />
-            <InputGroupAddon addonType="append">
-                <InputGroupText>'</InputGroupText>
-            </InputGroupAddon>
-            </InputGroup>
-        </Col>
-        <Col className="mb-1 pl-0">
-            <InputGroup size="small">
-            <Input
-                className="pr-0"
-                id="seconds"
-                name="seconds"
-                value={seconds}
-                onChange={handleSecondChange}
-                placeholder="Seconds"
-            />
-            <InputGroupAddon addonType="append">
-                <InputGroupText>"</InputGroupText>
-            </InputGroupAddon>
-            </InputGroup>
-        </Col>
+            <SingleInput classes="mb-1 pr-0" inputType="Degrees" inputSymbol="&#176;" inputValue={degrees} handleValueChange={handleDegreeChange} />
+            <SingleInput classes="mb-1 px-0 px-sm-1 px-md-2" inputType="Minutes" inputSymbol="'" inputValue={minutes} handleValueChange={handleMinuteChange} />
+            <SingleInput classes="mb-1 pl-0" inputType="Seconds" inputSymbol='"' inputValue={seconds} handleValueChange={handleSecondChange} />
         </>
+    );
+}
+
+function SingleInput(props) {
+    return (
+        <Col className={props.classes}>
+            <InputGroup size="small">
+                <Input
+                    className="pr-0"
+                    value={props.inputValue}
+                    onChange={props.handleValueChange}
+                    placeholder={props.inputType}
+                />
+                <InputGroupAddon addonType="append">
+                    <InputGroupText>{props.inputSymbol}</InputGroupText>
+                </InputGroupAddon>
+            </InputGroup>
+        </Col>
     );
 }
