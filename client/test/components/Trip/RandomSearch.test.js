038221bbea85cfd781fa3b5b1fc5436717993c9a
Derek McCracken <dsmccracken@outlook.com>
2021-09-28 08:13:06 -0600

    add RandomSearch tests

---
+++ client/test/components/Trip/RandomSearch.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { MOCK_FIND_RESPONSE} from '../../sharedMocks';
+import RandomSearch from '../../../src/components/Trip/Search/SearchOptions/RandomSearch';
+
+describe('RandomSearch', () => {
+    const setSearchResults = jest.fn();
+    
+    beforeEach(() => {
+        jest.clearAllMocks();
+        fetch.resetMocks();
+        fetch.mockResponse(MOCK_FIND_RESPONSE);
+        render(<RandomSearch currentURL="" setSearchResults={setSearchResults} activeTab="randomSearch" />);
+    });
+
+
+    it('sets results on button click', async () => {
+        fireEvent.click(screen.getByText('Find Random Places'));
+        await waitFor(() => {
+            expect(setSearchResults).toHaveBeenCalled();
+        });
+    });
+
+    it("does not set search results when no api response is received", () => {
+        fetch.mockResponseOnce(null);
+        fireEvent.click(screen.getByText('Find Random Places'));
+        expect(setSearchResults).toHaveBeenCalledTimes(0);
+    })
+});
