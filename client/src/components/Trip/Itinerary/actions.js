3cb9335f63f5f811a0a4d8bf925d6f6297b6780c
CalebCluett <ccluett@colostate.edu>
2021-10-05 14:34:23 +0000

    add

--- client/src/components/Trip/Itinerary/actions.js
+++ client/src/components/Trip/Itinerary/actions.js
@@ -7,12 +7,9 @@ import { currentLocation } from '../../../utils/currentLocation';
 
 export function ItineraryActionsDropdown(props) {
 
-    const [latitude, setCurrLatitude] = useState(DEFAULT_STARTING_PLACE.latitude);
-    const [longitude, setCurrLongitude] = useState(DEFAULT_STARTING_PLACE.longitude);
-
     return (
         <ActionsDropdown {...props}>
-            <DropdownItem onClick={() => {currentLocation(props.showMessage,setCurrLatitude,setCurrLongitude); props.placeActions.append({latitude,longitude});}} data-testid='home-button'>
+            <DropdownItem onClick={() => {currentLocation(props.showMessage,props.placeActions.append);}} data-testid='home-button'>
                 <FaHome />
             </DropdownItem>
             <DropdownItem onClick={() => props.placeActions.removeAll()} data-testid='delete-all-button'>
--- client/src/utils/currentLocation.js
+++ client/src/utils/currentLocation.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
 import { DEFAULT_STARTING_PLACE } from "./constants";
 
-export function currentLocation (showMessage,setCurrLatitude,setCurrLongitude){
+export function currentLocation (showMessage,append){
 
-    
-    if (!window.navigator.geolocation){
-    }else{
-            window.navigator.geolocation.getCurrentPosition(
-                position => {
-                    setCurrLatitude(position.coords.latitude);
-                    setCurrLongitude(position.coords.longitude);
-                },
-                error => {
-                    let message = error.message;
-                    showMessage(message,"warning");
-                }
-                );
-    }
-            
+    const [latitude, setCurrLatitude] = useState(DEFAULT_STARTING_PLACE.latitude);
+    const [longitude, setCurrLongitude] = useState(DEFAULT_STARTING_PLACE.longitude);
+
+    window.navigator.geolocation.getCurrentPosition(
+        position => {
+            setCurrLatitude(position.coords.latitude);
+            setCurrLongitude(position.coords.longitude);
+            append({latitude,longitude});
+        },
+        error => {
+            let message = error.message;
+            showMessage(message,"warning");
+        }
+    );          
 };
