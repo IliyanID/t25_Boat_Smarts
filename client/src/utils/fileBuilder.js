cf836409dd12e89e45f7f616f7dc70cd6cac43d9
Andrew Holmes <holmesaw@rams.colostate.edu>
2021-10-14 01:29:28 +0000

    build CSV string

--- client/src/utils/fileBuilder.js
+++ client/src/utils/fileBuilder.js
@@ -1,15 +1,36 @@
-import { latLngToPlace } from "./transformers"
-
 export function buildTripJSON(places, units, earthRadius) {
 
     let convertedPlaces = [];
-    places.map((place) => {convertedPlaces.push(latLngToPlace(place))});
+    for (const place of places) {
+        convertedPlaces.push({
+            latitude: place.lat.toString(),
+            longitude: place.lng.toString(),
+            name: place.name
+        });
+    }
 
     const tripJSON = {
         earthRadius: earthRadius,
         units: units,
-        places: convertedPlaces,
+        places: convertedPlaces
     };
 
     return JSON.stringify(tripJSON, null, 2);
+}
+
+export function buildTripCSV(places, units, earthRadius) {
+    let retStr = '"earthRadius","units","latitude","longitude", "name"';
+    for (const place of places) {
+        retStr += singleCSVRow(earthRadius, units, place.lat, place.lng, place.name);
+    }
+    return retStr;
+}
+
+function singleCSVRow(earthRadius, units, latitude, longitude, name){
+    let retStr = '\n' + earthRadius + ',';
+    retStr += '"' + units + '",';
+    retStr += '"' + latitude + '",';
+    retStr += '"' + longitude + '",';
+    retStr += '"' + name + '"';
+    return retStr;
 }
