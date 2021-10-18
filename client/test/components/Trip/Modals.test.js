bd381f1320c32ec6ccd409595f9f855fbd8341fa
CalebCluett <ccluett@colostate.edu>
2021-10-18 23:43:56 +0000

    add test

--- client/test/components/Trip/Modals.test.js
+++ client/test/components/Trip/Modals.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, screen, fireEvent} from '@testing-library/react';
 import {describe, expect, it, jest} from '@jest/globals';
 import FileUploadModal from '../../../src/components/Trip/Itinerary/Modals/FileUploadModal';
+import FileDownloadModal from '../../../src/components/Trip/Itinerary/Modals/FileDownloadModal';
 
 describe('FileUploadModal', () => {
     it('renders to screen', () => {
@@ -10,5 +11,15 @@ describe('FileUploadModal', () => {
 
     it('does not render when fileUploadOpen is false', () => {
         render(<FileUploadModal fileUploadOpen={false} />);
+    });
+});
+
+describe('FileDownloadModal', () => {
+    it('renders to screen', () => {
+        render(<FileDownloadModal />);
+    });
+
+    it('does not render when fileUploadOpen is false', () => {
+        render(<FileDownloadModal fileUploadOpen={false} />);
     });
 });
