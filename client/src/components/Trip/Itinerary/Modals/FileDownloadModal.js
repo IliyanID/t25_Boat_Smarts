f88eaef4c95dafdf01e2c4b3347a56ede54052dc
CalebCluett <ccluett@colostate.edu>
2021-10-19 14:43:56 +0000

    add

--- client/src/components/Trip/Itinerary/Modals/FileDownloadModal.js
+++ client/src/components/Trip/Itinerary/Modals/FileDownloadModal.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
-import { Button, Input, InputGroup, InputGroupAddon,InputGroupText, Modal, ModalBody, ModalFooter, ModalHeader, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import { Button, Input, InputGroup, InputGroupAddon,InputGroupText, Modal, ModalBody, ModalFooter, ModalHeader, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Form, FormGroup, Label, Row, Col } from "reactstrap";
 
 export default function FileDownloadModal(props) {
 
     const [dropdownOpen,setDropdownOpen] = useState(false);
 
     const toggle = () => setDropdownOpen(prevState => !prevState);
+
+    const [saveToMem, setSaveToMem] = useState(false);
 
     return (
         <Modal isOpen={props.fileDownloadOpen} toggle={props.toggleFileDownloadOpen}>
@@ -17,16 +19,21 @@ export default function FileDownloadModal(props) {
                     </InputGroupAddon>
                     <Input type="textarea" placeholder="Trip Name" onChange={text => localStorage.setItem("tripName",text)}/>
                 </InputGroup><br/>
-
-                <Dropdown direction="right" isOpen={dropdownOpen} toggle={toggle}>
-                    <DropdownToggle caret>
-                        {localStorage.getItem("fileType")}
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem onClick={()=> localStorage.setItem("fileType","JSON")}>JSON</DropdownItem>
-                        <DropdownItem onClick={()=> localStorage.setItem("fileType","CSV")}>CSV</DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
+                <Form>
+                    <Dropdown direction="right" isOpen={dropdownOpen} toggle={toggle}>
+                        <DropdownToggle caret>
+                            {localStorage.getItem("fileType")}
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem onClick={()=> {saveToMem ? localStorage.setItem("fileType","JSON") : {}}}>JSON</DropdownItem>
+                            <DropdownItem onClick={()=> {saveToMem ? localStorage.setItem("fileType","CSV"): {}}}>CSV</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown><br/>
+                    <FormGroup check>
+                        <Input type="checkbox" onClick={setSaveToMem}/>
+                        <Label>Save Settings For Later</Label>
+                    </FormGroup>
+                </Form>
             </ModalBody>
             <ModalFooter>
                 <Button color="secondary" onClick={props.toggleFileDownloadOpen}>Cancel</Button>
