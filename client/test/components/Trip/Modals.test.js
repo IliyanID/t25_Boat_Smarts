8ff40f16491cd68e5f82c27c449f9d8eb81ad807
holmesaw <holmesaw@rams.colostate.edu>
2021-10-20 11:10:41 -0600

    Revert "some tests"
    This reverts commit 5e459c56b29ccdf3e9c236459673ee84db1ece8f.

--- client/test/components/Trip/Modals.test.js
+++ client/test/components/Trip/Modals.test.js
@@ -2,13 +2,7 @@ import React from 'react';
 import {render, screen, fireEvent} from '@testing-library/react';
 import {describe, expect, it, jest} from '@jest/globals';
 import FileUploadModal from '../../../src/components/Trip/Itinerary/Modals/FileUploadModal';
-import FileDownloadModal, {buildFileText, addExtension, downloadFile} from '../../../src/components/Trip/Itinerary/Modals/FileDownloadModal';
-import {buildTripJSON, buildTripCSV} from "../../../src/utils/fileBuilder";
-
-jest.mock('../../../src/utils/fileBuilder', ()=>({
-    buildTripJSON: ()=>'mock JSON',
-    buildTripCSV: ()=>'mock CSV'
-}));
+import FileDownloadModal from '../../../src/components/Trip/Itinerary/Modals/FileDownloadModal';
 
 describe('FileUploadModal', () => {
     it('renders to screen', () => {
@@ -21,7 +15,6 @@ describe('FileUploadModal', () => {
 });
 
 describe('FileDownloadModal', () => {
-
     it('renders to screen', () => {
         render(<FileDownloadModal />);
     });
@@ -29,21 +22,4 @@ describe('FileDownloadModal', () => {
     it('does not render when fileUploadOpen is false', () => {
         render(<FileDownloadModal fileUploadOpen={false} />);
     });
-
-    it('creates correct JSON file names', () => {
-        expect(addExtension("my trip", "application/json")).toEqual("my_trip.json");
-    });
-
-    it('creates correct CSV file names', () => {
-        expect(addExtension("my trip", "text/csv")).toEqual("my_trip.csv");
-    });
-
-    it('uses buildTripJSON to build JSON Text', () => {
-        expect(buildFileText("application/json", {})).toEqual("mock JSON");
-    });
-
-    it('uses buildTripCSV to build CSV Text', () => {
-        expect(buildFileText("text/csv", {})).toEqual("mock CSV");
-    });
-
 });
