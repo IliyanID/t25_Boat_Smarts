45fe9841957c00be1242941098a2730af9e57b1c
iliyanid <dev.iliyan.dimitrov@gmail.com>
2021-10-27 01:10:59 -0600

    Improved Code Climate

--- client/src/components/Trip/Itinerary/Body/TableRow/DistanceInfo/DistanceInfo.js
+++ client/src/components/Trip/Itinerary/Body/TableRow/DistanceInfo/DistanceInfo.js
@@ -3,20 +3,19 @@ import React from 'react'
 const DistanceInfo = (props) =>{
     let individualItem = props.cumalitiveDistances[props.index]
     let distances = props.distances.distances
-    
+
     const unit = (item) =>{
         if(item == 1)
             return item + ' mile'
    
         return item + ' miles'
     }
-
-    let individualDistanceText = 'Distance to Start'
-    if(props.index === distances.length - 1)
-        individualDistanceText = 'Distance to Place #' + (props.index + 2)
-
     
     if(distances && distances.length > props.index && distances.length !== 1){
+        let individualDistanceText = 'Distance to Start'
+        if(props.index === distances.length - 1)
+            individualDistanceText = 'Distance to Place #' + (props.index + 2)
+            
         return(
             <small>
                 <th>  
