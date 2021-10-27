d26b1583859f06cbe6858e4d22d109daa7126492
iliyanid <dev.iliyan.dimitrov@gmail.com>
2021-10-27 01:17:14 -0600

    improved code climate

--- client/src/components/Trip/Itinerary/Body/TableRow/DistanceInfo/DistanceInfo.js
+++ client/src/components/Trip/Itinerary/Body/TableRow/DistanceInfo/DistanceInfo.js
@@ -11,24 +11,24 @@ const DistanceInfo = (props) =>{
         return item + ' miles'
     }
     
-    if(distances && distances.length > props.index && distances.length !== 1){
-        let individualDistanceText = 'Distance to Start'
-        if(props.index === distances.length - 1)
-            individualDistanceText = 'Distance to Place #' + (props.index + 2)
-            
-        return(
-            <small>
-                <th>  
-                    {individualDistanceText} : {unit(individualItem.distance)} 
-                </th>
+    if(!distances || !(distances.length > props.index) || !(distances.length !== 1))
+        return <></>
 
-                <th>
-                    Cumulative Distance : {unit(individualItem.total)}
-                </th>
-            </small>
-        )
-    }
-    return <></>
+    let individualDistanceText = 'Distance to Place #' + (props.index + 2)
+    if(props.index === distances.length - 1)
+        individualDistanceText = 'Distance to Start'
+
+    return(
+        <small>
+            <th>  
+                {individualDistanceText} : {unit(individualItem.distance)} 
+            </th>
+
+            <th>
+                Cumulative Distance : {unit(individualItem.total)}
+            </th>
+        </small>
+    ) 
 }
 
 export default DistanceInfo;
