82f98d86c3256dcc083020d3a44878c586897205
Andrew Holmes <holmesaw@rams.colostate.edu>
2021-10-27 18:11:43 +0000

    add tour endpoint

---
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -0,0 +1,51 @@
+package com.tco.requests;
+
+import java.util.ArrayList;
+
+import com.tco.database.SQLDatabase;
+import com.tco.misc.DistanceCalculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+
+public class TourRequest extends Request {
+    private SQLDatabase.Places places;
+    private double earthRadius;
+    private double response;
+    private final transient Logger log = LoggerFactory.getLogger(DistancesRequest.class);
+
+    @Override
+    public void buildResponse() {
+        // do the optim
+        log.trace("buildResponse -> {}", this);
+    }
+
+    // Testing methods
+
+    public TourRequest() {
+        this.requestType = "tour";
+    }
+
+    public SQLDatabase.Places getPlaces() {
+        return places;
+    }
+    public void setPlaces(SQLDatabase.Places places) {
+        this.places = places;
+    }
+
+    public double getEarthRadius() {
+        return earthRadius;
+    }
+    public void setEarthRadius(double earthRadius) {
+        this.earthRadius = earthRadius;
+    }
+
+    public double getResponse() {
+        return response;
+    }
+    public void setResponse(double response){
+        this.response = response;
+    }
+}
