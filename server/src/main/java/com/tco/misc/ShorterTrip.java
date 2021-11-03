ebc99f95654cc21d3ad64e0795134f288ecbc81d
iliyanid <dev.iliyan.dimitrov@gmail.com>
2021-11-02 18:41:07 -0600

    Fixed duplicate places bug

--- server/src/main/java/com/tco/misc/ShorterTrip.java
+++ server/src/main/java/com/tco/misc/ShorterTrip.java
@@ -30,10 +30,12 @@ public class ShorterTrip {
         for (int i = 0; i < this.distances.length; i++) {
             for (int j = 0; j < this.distances[i].length; j++) {
                 this.distances[i][j] = DistanceCalculator.singleDistance(places.get(i), places.get(j), earthRadius);
+                //System.out.print(this.distances[i][j] + " ");
                 if(this.distances[i][j] == 0){
                     this.distances[i][j] = Integer.MAX_VALUE;
                 }
             }
+            //System.out.println();
         }
     }
 
@@ -41,8 +43,8 @@ public class ShorterTrip {
         double minDistance = Double.POSITIVE_INFINITY;
 
 
-
         for (int i = 0; i < this.distances.length; i++) {
+            //System.out.println("\n\nStarting Location: " + i);
             int[] testShortTrip = new int[this.shorterTrip.length];
             this.visited = new boolean[this.shorterTrip.length];
             double newMinDistance = 0;
@@ -50,6 +52,7 @@ public class ShorterTrip {
 
             int currentPlace = i;
             testShortTrip[0] = currentPlace;
+            this.visited[currentPlace] = true;
             for(int j = 1; j < this.distances.length;j++){
                 int nextClosestPlace = findClosestPlace(currentPlace);
                 if(nextClosestPlace >= 0){
@@ -57,9 +60,11 @@ public class ShorterTrip {
                     testShortTrip[j] = nextClosestPlace;
                     newMinDistance += this.distances[currentPlace][nextClosestPlace];
                     currentPlace = nextClosestPlace;
+
                 }
                 
             }
+            //System.out.println("Temp Running Total:" + newMinDistance);
             if(newMinDistance < minDistance){
                 minDistance = newMinDistance;
                 this.shorterTrip = testShortTrip;
@@ -84,11 +89,13 @@ public class ShorterTrip {
 
 
         for (int i = 0; i < this.distances[currPlace].length; i++) {
+            System.out.println(this.distances[currPlace][i] + " < " + shortestDistance + " | Visited: " + this.visited[i] );
             if (this.distances[currPlace][i] < shortestDistance && !this.visited[i]) {
                 shortestDistance = this.distances[currPlace][i];
                 closestPlace = i;
             }
         }
+        //System.out.println("closest to" + currPlace + " is " + closestPlace + "\n");
         return closestPlace;
     }
 
