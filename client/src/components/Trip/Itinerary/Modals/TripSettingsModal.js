1020fe03bde1bc18c3626028068bb3e26afd515e
iliyanid <dev.iliyan.dimitrov@gmail.com>
2021-11-15 16:16:28 -0700

    Added TripSettingsModal.js

---
+++ client/src/components/Trip/Itinerary/Modals/TripSettingsModal.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+import Switch from 'react-switch'
+import { useToggle } from '../../../../hooks/useToggle.js';
+import { Button, 
+     Modal, ModalBody, ModalFooter, ModalHeader, Row, Col, Container } from "reactstrap";
+
+export function TripSettingsModal(props) {
+    const handleAutoTour = ()=>{
+        props.toggleAutomaticallyRunTour()
+        if(!props.disablePreviewMode){
+            props.toggleDisablePreviewMode();
+        }
+    }
+    return (
+        <Modal isOpen={props.tripSettingsOpen} toggle={props.toggleTripSettingsOpen}>
+            <ModalHeader toggle={props.toggleTripSettingsOpen}>Trip Settings</ModalHeader>
+            <Container>
+                <Row>
+                     <Col><Switch onChange={handleAutoTour} checked={props.automaticallyRunTour}/></Col>
+                     <Col>Automatically Shorten Trip</Col>
+                </Row>
+                <Row>
+                    <Col><Switch disabled={props.automaticallyRunTour}  onChange={props.toggleDisablePreviewMode} checked={props.disablePreviewMode}/></Col>
+                    <Col>Disable Preview Mode</Col>
+                </Row>
+            </Container>
+            <ModalFooter>
+                <Button color="primary" onClick={props.toggleTripSettingsOpen} data-testid="SaveSettings">Save</Button>
+            </ModalFooter>
+        </Modal>
+    )
+}
+
+export default TripSettingsModal
