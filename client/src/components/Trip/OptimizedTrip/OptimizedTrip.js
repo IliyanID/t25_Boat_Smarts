1af551d56c83dd7a5ffa452da0e6c43d770d7317
iliyanid <dev.iliyan.dimitrov@gmail.com>
2021-11-15 16:38:24 -0700

    Reset Optimized Trip

--- client/src/components/Trip/OptimizedTrip/OptimizedTrip.js
+++ client/src/components/Trip/OptimizedTrip/OptimizedTrip.js
@@ -4,63 +4,44 @@ import { Button, Tooltip } from 'reactstrap';
 import { AiFillInfoCircle } from 'react-icons/ai';
 import '../../../static/styles/focus.css'
 
-const packageRefs = () =>{
+const OptimizedTrip = (props) =>{
     let firstMount = useRef(true);
     let blockerRef = useRef();
     let buttonsRef = useRef();
-    let [toolTip,toggleToolTip] = useToggle(false)
-    return {
-        firstMount,
-        blockerRef,
-        buttonsRef,
-        toolTip,toggleToolTip
+    const hanleConfirm = () =>{
+        props.togglePreviewTripFocus()
     }
-}
+    
+    const handleReject = () =>{
+        props.togglePreviewTripFocus();
+        props.setAllPlaces(props.origionalPlaces)
+    }
 
-const hanleConfirm = (allPackages) =>{
-    if(allPackages.previewTripFocus)
-        allPackages.togglePreviewTripFocus()
-}
+    let [toolTip,toggleToolTip] = useToggle(false)
 
-const handleReject = (allPackages) =>{
-    allPackages.togglePreviewTripFocus();
-    allPackages.setAllPlaces(allPackages.origionalPlaces)
-}
-
-const handlePreviewFocus = (allPackages)=>{
-    return    useEffect(()=>{
-        if(allPackages.firstMount.current)
-            allPackages.firstMount.current = false;
-        else if(allPackages.previewTripFocus && !allPackages.disablePreviewMode){
-            allPackages.blockerRef.current.classList = 'focus';
-            allPackages.buttonsRef.current.classList = 'OptimizationOption'
+    useEffect(()=>{
+        if(firstMount.current)
+            firstMount.current = false;
+        else if(props.previewTripFocus){
+            blockerRef.current.classList = 'focus';
+            buttonsRef.current.classList = 'OptimizationOption'
         }
         else{
-            allPackages.blockerRef.current.classList = 'notInFocus';
-            allPackages.buttonsRef.current.classList = 'OptimizationOptionHide'
+            blockerRef.current.classList = 'notInFocus';
+            buttonsRef.current.classList = 'OptimizationOptionHide'
         }
-    },[allPackages.previewTripFocus])
+    },[props.previewTripFocus])
 
-}
-
-const OptimizedTrip = (props) =>{
-    const allRefs = packageRefs()
-    const allPackages ={...allRefs,...props}
-    handlePreviewFocus(allPackages) 
-
-
-    if(allPackages.disablePreviewMode)
-            hanleConfirm(allPackages)
     return  <>
-                <div ref={allPackages.blockerRef} data-testid='blocker'/>
-                <div ref={allPackages.buttonsRef} className='OptimizationOptionDefault'>
+                <div ref={blockerRef} data-testid='blocker'/>
+                <div ref={buttonsRef} className='OptimizationOptionDefault'>
                         <div className='OptimizedHeader'>
                             <h3>Planner is in Preview Mode</h3>
                             <AiFillInfoCircle id='previewMode'/>
-                            <Tooltip placement="right" isOpen={allPackages.toolTip} toggle={allPackages.toggleToolTip} target='previewMode'>All site features, except for the map, are temporarily disabled until the user confirms or denies the optimized trip.</Tooltip>
+                            <Tooltip placement="right" isOpen={toolTip} toggle={toggleToolTip} target='previewMode'>All site features, except for the map, are temporarily disabled until the user confirms or denies the optimized trip.</Tooltip>
                         </div>
-                        <Button color="primary" onClick={()=>hanleConfirm(allPackages)} data-testid='ConfirmTrip'>Confirm Optimized Trip</Button>
-                        <Button color="secondary" onClick={()=>handleReject(allPackages)} data-testid='DenyTrip'>Revert to Origional Trip</Button>
+                        <Button color="primary" onClick={hanleConfirm} data-testid='ConfirmTrip'>Confirm Optimized Trip</Button>
+                        <Button color="secondary" onClick={handleReject} data-testid='DenyTrip'>Revert to Original Trip</Button>
                 </div>
             </>
 }
