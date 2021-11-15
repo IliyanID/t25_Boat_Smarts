2d52885d5d0c9cf9e799636995ef113096d7dc78
iliyanid <dev.iliyan.dimitrov@gmail.com>
2021-11-15 15:45:42 -0700

    Reset tripname

--- client/src/components/Trip/Itinerary/TripName/TripName.js
+++ client/src/components/Trip/Itinerary/TripName/TripName.js
@@ -3,92 +3,66 @@ import React, { useState, useRef, useEffect, useToggle } from 'react'
 import { FaEdit } from 'react-icons/fa';
 import CheckMark from '../../../../static/images/checkmark.svg'
 import Cancel from '../../../../static/images/cancel.svg'
-import { NavItem } from 'reactstrap';
 
-const packageTripName = (props) =>{
+const TripName = (props) =>{
     const inputRef = useRef();
     const [tempName,setTempName] = useState(props.tripName)
     const [inFocus,setInFocus] = useState(false)
-    return {
-        inputRef:inputRef,
-        tempName:tempName,setTempName:setTempName,
-        inFocus:inFocus,setInFocus:setInFocus
-    }
-}
-let handleFocusOut = (e,allPackages)=>{
-    let userDidntPressCheckMark = e && e.path && e.path[1].id !== "inputContainer";
-    let userDidntPressSaveButton = e && e.path && e.path[0].innerText != "Save"
-    //If the parent of the clicked item isn't the inputRef div
-    if(userDidntPressCheckMark && userDidntPressSaveButton){
-        handleSubmit(allPackages); 
-    }
-}
 
-const setFocus = (allPackages) =>{
-    allPackages.setInFocus(true)
-    allPackages.inputRef.current.focus()
-}
-const handleCancel = (allPackages) =>{
-    allPackages.setInFocus(false)
-    allPackages.setTempName(allPackages.tripName)
-}
-const handleSubmit = (allPackages) =>{
-    allPackages.setInFocus(false)
-    if(allPackages.inputRef.current && allPackages.tripName !== allPackages.inputRef.current.value){
-        printMessage(allPackages);
-        allPackages.setTripName(allPackages.inputRef.current.value)
+
+
+    let handleFocusOut = (e)=>{
+        let userDidntPressCheckMark = e && e.path && e.path[1].id !== "inputContainer";
+        let userDidntPressSaveButton = e && e.path && e.path[0].innerText != "Save"
+        //If the parent of the clicked item isn't the inputRef div
+        if(userDidntPressCheckMark && userDidntPressSaveButton){
+            handleSubmit(); 
+        }
     }
-}
-
-const printMessage = (allPackages) =>{
-    let message = "Trip Name has been changed from \'" + allPackages.tripName + "\' to \'" + allPackages.inputRef.current.value + "\'.";
-    allPackages.showMessage(message,"info");
-}
-let cancelPackage = [
-    {
-        id:'submitName',
-        onClick:(allPackages)=>handleSubmit(allPackages),
-        src:CheckMark
-    },
-    {
-        id:'cancelName',
-        onClick:(allPackages)=>handleCancel(allPackages),
-        src:Cancel
-    }
-]
-
-const getButtonLayout = (allPackages) =>{
-    let iconStyle = {width:"20px",cursor:"pointer",marginRight:"10px"}
-    let buttonLayout;
-
-    if(!allPackages.inFocus){
-        //Shift to the right to make room for the cancel button when rendered so the input doesn't move around
-        iconStyle["marginLeft"] = "30px"
-        buttonLayout = <FaEdit  data-testid="edit" onClick={()=>setFocus(allPackages)} style={iconStyle}/>
-    }
-    else{
-        buttonLayout = cancelPackage.map(item=>{
-            return <img data-testid={item.id} style={iconStyle} onClick={()=>item.onClick(allPackages)} src={item.src}></img>
-        })
-    }
-    return buttonLayout;
-}
-
-const TripName = (props) =>{
-    const states = packageTripName(props)
-    const allPackages = {...states,...props}
-
     useEffect(() => {
-        document.addEventListener('click', (e)=>handleFocusOut(e,allPackages))
+        document.addEventListener('click', handleFocusOut)
     },[])
 
-    const buttonLayout = getButtonLayout(allPackages)
-    
+    const setFocus = () =>{
+        setInFocus(true)
+        inputRef.current.focus()
+    }
+    const handleCancel = () =>{
+        setInFocus(false)
+        setTempName(props.tripName)
+    }
+    const handleSubmit = () =>{
+        setInFocus(false)
+        if(props.tripName !== inputRef.current.value){
+            printMessage();
+            props.setTripName(inputRef.current.value)
+        }
+    }
+
+    const printMessage = () =>{
+        let message = "Trip Name has been changed from \'" + props.tripName + "\' to \'" + inputRef.current.value + "\'.";
+        props.showMessage(message,"info");
+    }
+
+    let iconStyle = {width:"20px",cursor:"pointer",marginRight:"10px"}
+
+    let buttonLayout;
+    if(!inFocus){
+        //Shift to the right to make room for the cancel button when rendered so the input doesn't move around
+        iconStyle["marginLeft"] = "30px"
+        buttonLayout = <FaEdit  data-testid="edit" onClick={setFocus} style={iconStyle}/>
+    }
+    else{
+        buttonLayout = (<>
+            <img data-testid="submitName" id="submit" style={iconStyle} onClick={handleSubmit} src={CheckMark} />
+            <img data-testid="cancelName" id="cancel" style={iconStyle} onClick={handleCancel} src={Cancel} />
+        </>)
+    }
 
     return(
-        <div style={allPackages.style} id="inputContainer">
+        <div id="inputContainer">
             {buttonLayout}
-            <input data-testid="input" ref={allPackages.inputRef} onFocus={()=>setFocus(allPackages)} style={{border:"none"}} type="text" onChange={(e)=>allPackages.setTempName(e.target.value)} value={allPackages.tempName}/>
+            <input data-testid="input" ref={inputRef} onFocus={setFocus} style={{border:"none"}} type="text" onChange={(e)=>setTempName(e.target.value)} value={tempName}/>
         </div>
     )
     
