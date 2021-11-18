28e1f139e4e3450d4c5a7c8607f3ea6bf0a8fa18
iliyanid <dev.iliyan.dimitrov@gmail.com>
2021-11-18 14:33:06 -0700

    All Layers on sreen

--- client/src/components/Trip/Map/LayerSelection.js
+++ client/src/components/Trip/Map/LayerSelection.js
@@ -1,24 +1,35 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import '../../../static/styles/layer-selection.scss'
 import { Map as LeafletMap, TileLayer} from 'react-leaflet';
 
-export const LayerSelection = (props) =>{
-    const map = useRef()
-
+const IndividualLayer = (props) =>{
     return(
         <div className='layerSelection'>
-            <div onClick={()=>props.setSelectedLayer(props.layers.satelite)} className='disablePreviewMap'>Layers</div>
+            <div onClick={()=>props.setSelectedLayer(props.layers[props.index])} className='disablePreviewMap'>Layers</div>
             <LeafletMap 
                 className='previewMap'
                 zoom={1}
                 center={props.coordinates}
                 zoomControl={false} 
-                ref={map} 
                 attributionControl={false} 
             >
-                    <TileLayer url={props.layers.satelite}/>
+                    <TileLayer url={props.layers[props.index]}/>
             </LeafletMap>
         </div>
+ 
     )
+}
+
+export const LayerSelection = (props) =>{
+
+    return(
+        <>
+        {
+            Object.keys(props.layers).map(item=>{
+                return <IndividualLayer index={item}  {...props}/>
+            })
+        }
+        </>
+   )
 }
 export default LayerSelection;
