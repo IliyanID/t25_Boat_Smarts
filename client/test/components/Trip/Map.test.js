e9e15c7fea356cfd27d7e02f04592525d50bcbc6
IliyanID <dev.iliyan.dimitrov@gmail.com>
2021-11-19 12:42:26 -0700

    Fixed npm package

--- client/test/components/Trip/Map.test.js
+++ client/test/components/Trip/Map.test.js
@@ -16,6 +16,24 @@ describe('Map', () => {
 
     beforeAll(() => {
         Object.defineProperty(window, 'scrollTo', { value: () => {}, writable: true });
+        var localStorageMock = (function() {
+            var store = {};
+            return {
+              getItem: function(key) {
+                return store[key];
+              },
+              setItem: function(key, value) {
+                store[key] = value.toString();
+              },
+              clear: function() {
+                store = {};
+              },
+              removeItem: function(key) {
+                delete store[key];
+              }
+            };
+          })();
+          Object.defineProperty(window, 'localStorage', { value: localStorageMock });
     });
 
 
