5972e0ce40980afe2718ba712e2c54c2d0a77555
Derek McCracken <dsmccracken@outlook.com>
2021-12-08 11:17:51 -0700

    add dashed line option to planner

--- client/src/components/Trip/Planner.js
+++ client/src/components/Trip/Planner.js
@@ -93,6 +93,7 @@ const packageTripSettings = () =>{
     p = packageStatesIntoObject(p,['fileUnitsName', 'setFileUnitsName'], useState(localStorage.getItem("fileUnitsName") != null ? localStorage.getItem("fileUnitsName") : "miles"))
     p = packageStatesIntoObject(p,['fileUnitsValue', 'setFileUnitsValue'], useState(localStorage.getItem("fileUnitsValue") != null ? localStorage.getItem("fileUnitsValue") : 3959.0))
     p = packageStatesIntoObject(p,['lineColor', 'setLineColor'], useState('#3388ff'))
+    p = packageStatesIntoObject(p,['dashArray', 'setDashArray'], useState(""))
     return p;
 }
 
