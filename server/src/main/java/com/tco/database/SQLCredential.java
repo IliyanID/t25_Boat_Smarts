207c219bf41d6688cc8178ef78e33d0a32c2f68f
IliyanID <dev.iliyan.dimitrov@gmail.com>
2021-12-10 03:45:56 -0700

    creds

--- server/src/main/java/com/tco/database/SQLCredential.java
+++ server/src/main/java/com/tco/database/SQLCredential.java
@@ -1,11 +1,11 @@
 package com.tco.database;
 
 public class SQLCredential {
-    final static String USER = "b574990a72e725";
-        final static String PASSWORD = "181319bb";
+    final static String USER = "user";
+        final static String PASSWORD = "password";
         
         static String url() {
-            String URL = "jdbc:mariadb://us-cdbr-east-05.cleardb.net/heroku_001ba428b240e57";
+            String URL = "jdbc:mariadb://lindibracelets.com/cs314";
   
 
             //mysql://b574990a72e725:181319bb@us-cdbr-east-05.cleardb.net/heroku_001ba428b240e57?reconnect=true
